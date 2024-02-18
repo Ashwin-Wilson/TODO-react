199a808286dff3a11a6118a39483ef484e2b947b
Ashwin Wilson <ashwinwilson24@gmail.com>
2024-02-18 19:33:10 +0530

    initial commit stable version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  let [toDos, setToDos] = useState([]);
+  let [toDo, setToDo] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+
+      </div>
+      <div className="input">
+        <input value={toDo} onChange={(e) => setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={() => setToDos([...toDos, { ID: Date.now(), text: toDo, activeStatus: false, cancelStatus: false }])} className="fas fa-plus"></i>
+      </div>
+
+      <div className="todos">
+        {toDos.map((obj, index) => {
+          if (obj.activeStatus === false && obj.cancelStatus === false) {
+            return (
+              <div className="todo">
+                <div className="left">
+                  <input onChange={(e) => {
+                    setToDos(toDos.filter((obj2) => {
+                      if (obj2.ID === obj.ID) {
+                        obj2.activeStatus = e.target.checked;
+                      }
+                      return obj2;
+                    }))
+                  }} type="checkbox" name="" id="" />
+                  <p>{obj.text}</p>
+                </div>
+                <div className="right">
+                  <i onClick={()=>{
+                    setToDos(
+                      toDos.filter((obj2) => {
+                        if (obj2.ID === obj.ID) {
+                          obj2.cancelStatus = true;
+                        }
+                        return obj2;
+                      })
+                    )
+                  }} className="fas fa-times"></i>
+                </div>
+              </div>
+            )
+          }
+
+        })}
+
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Completed</h2>
+
+      </div>
+      <div className='todos'>
+        {toDos.map((obj) => {
+          if (obj.activeStatus === true) {
+            return (
+              <div className='todo'>{obj.text}</div>
+            )
+          }
+          return null;
+        })}
+      </div>
+
+      <div className="subHeading">
+        <br />
+        <h2>Cancelled</h2>
+
+      </div>
+      <div className='todos'>
+        {toDos.map((obj) => {
+          if (obj.cancelStatus === true) {
+            return (
+              <div className='todo'>{obj.text}</div>
+            )
+          }
+          return null;
+        })}
+      </div>
+
+
+
     </div>
   );
 }
